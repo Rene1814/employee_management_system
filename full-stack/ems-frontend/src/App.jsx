7a4f567a7fbf89030b67b5b241b556e091d244d7
Rene Souza <rne_souza@yahoo.com.br>
2026-09-14 19:43:02 -0300

    Header & Footer components created

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import HeaderComponent from "./components/HeaderComponent"
 import ListEmployeeComponent from "./components/ListEmployeeComponent"
+import "../src/assets/styles.css"
+import FooterComponent from "./components/FooterComponent"
 
 function App() {
   return (
     <>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   )
 }
---
+++ full-stack/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand px-3" href="http://localhost:5173">Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
