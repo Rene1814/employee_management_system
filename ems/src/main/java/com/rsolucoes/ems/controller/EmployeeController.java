60fa642bfba9a0e18146e8baa1b683d4bce6c148
Rene Souza <rne_souza@yahoo.com.br>
2026-09-14 17:36:58 -0300

    updateEmployee REST API endpoint & methods created

--- ems/src/main/java/com/rsolucoes/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/rsolucoes/ems/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +45,13 @@ public class EmployeeController {
 	public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
 		List<EmployeeDto> employees = employeeService.getAllEmployees();
 		return ResponseEntity.ok(employees);
+	}
+	
+	//Build Update Employee REST API
+	@PutMapping("{id}")
+	public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, 
+														@RequestBody EmployeeDto updatedEmployee){
+		EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
+		return ResponseEntity.ok(employeeDto);
 	}
 }
