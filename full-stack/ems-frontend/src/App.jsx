6cffecfb0fe598e00ab19bc9060033f7797f26db
Rene Souza <rne_souza@yahoo.com.br>
2026-09-14 19:55:45 -0300

    Routing configure

--- full-stack/ems-frontend/src/App.jsx
+++ full-stack/ems-frontend/src/App.jsx
@@ -2,13 +2,21 @@ import HeaderComponent from "./components/HeaderComponent"
 import ListEmployeeComponent from "./components/ListEmployeeComponent"
 import "../src/assets/styles.css"
 import FooterComponent from "./components/FooterComponent"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent />
-      <ListEmployeeComponent />
+      <Routes>
+        {/* // http://localhost:5173 */}
+        <Route path="/" element={<ListEmployeeComponent />} />
+        {/* // http://localhost:5173/employees */}
+        <Route path="/employees" element={<ListEmployeeComponent />} />
+      </Routes>
       <FooterComponent />
+    </BrowserRouter>
     </>
   )
 }
