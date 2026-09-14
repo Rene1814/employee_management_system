2d2bb9aae5febe75225046970f973613fa1d3f1d
Rene Souza <rne_souza@yahoo.com.br>
2026-09-14 17:45:17 -0300

    deleteEmployee REST API endpoint & methods created

--- ems/src/main/java/com/rsolucoes/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/rsolucoes/ems/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import lombok.AllArgsConstructor;
 public class EmployeeController {
 
 	private EmployeeService employeeService;
+
 	
 	//Build Add Employee REST API
 	@PostMapping
@@ -50,8 +52,15 @@ public class EmployeeController {
 	//Build Update Employee REST API
 	@PutMapping("{id}")
 	public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, 
-														@RequestBody EmployeeDto updatedEmployee){
+													  @RequestBody EmployeeDto updatedEmployee){
 		EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
 		return ResponseEntity.ok(employeeDto);
+	}
+	
+	//Build Delete Employee REST API
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
+		employeeService.deleteEmployee(employeeId);
+		return ResponseEntity.ok("Employee deleted successfully!");
 	}
 }
--- ems/src/main/java/com/rsolucoes/ems/service/impl/EmployeeServiceImpl.java
+++ ems/src/main/java/com/rsolucoes/ems/service/impl/EmployeeServiceImpl.java
@@ -53,4 +53,15 @@ public class EmployeeServiceImpl implements EmployeeService{
 		return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
 	}
 
+	@SuppressWarnings("unused")
+	@Override
+	public void deleteEmployee(Long employeeId) {
+		
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+				() -> new ResourceNotFoundException("Employee is not exist with given id: " 
+						+ employeeId));
+		
+		employeeRepository.deleteById(employeeId);
+	}
+
 }
